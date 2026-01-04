d03d8bc94604d6036bbda8cd57690a1151b39a87
Andrew <aholding82@gmail.com>
2026-01-04 21:57:30 +1100

    finished the romanNumeral module

--- roman2.py
+++ roman2.py
@@ -15,6 +15,43 @@
 # can also use enumerate
 # 
 # Store the values of the roman numerals in a dictionary?
+# [start:stop:step]
 
 class Solution:
+    def __init__(self):
+        self.numerals = {"I": 1,
+                    "V": 5,
+                    "X": 10,
+                    "L": 50,
+                    "C": 100,
+                    "D": 500,
+                    "M": 1000,
+                    }
+
     def romantoInt(self,s):
+        self.s = s
+        total = 0
+        revs = self.s[::-1]
+        y = []
+        for x in revs:
+            y.append(self.numerals[x])
+            total = total + self.numerals[x]
+            if len(y) >= 2:
+                if (y[-1] == 1 and y[-2] == 5):
+                    total = total - 2
+                elif (y[-1] == 1 and y[-2] == 10):
+                    total = total - 2
+                elif (y[-1] == 10 and y[-2] == 50):
+                    total = total - 20
+                elif (y[-1] == 10 and y[-2] == 100):
+                    total = total - 20
+                elif (y[-1] == 100 and y[-2] == 500):
+                    total = total - 200
+                elif (y[-1] == 100 and y[-2] == 1000):
+                    total = total - 200
+        return total
+        
+input = 'XC'
+c = Solution()
+results = c.romantoInt(input)
+print(results)
